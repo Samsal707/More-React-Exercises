93677c91064f75b264f33e7bc8cbb3b9e7cc6352
Samsal707 <samsal707@gmail.com>
2022-12-29 18:39:30 +1100

    customised Fetch component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import React from "react";
-import { FixedSizeList } from "react-window";
-import faker from "faker";
-
-const bigList = [...Array(5000)].map(() => ({
-  name: faker.name.findName(),
-  email: faker.internet.email(),
-  avatar: faker.internet.avatar()
-}));
-
+import React, { useState } from "react";
+import GitHubUser from "./GitHubUser";
+// import SearchForm from "./SearchForm";
 
 export default function App() {
-  const renderRow = ({ index, style }) => (
-    <div style={{ ...style, ...{ display: "flex" } }}>
-      <img
-        src={bigList[index].avatar}
-        alt={bigList[index].name}
-        width={50}
-      />
-      <p>
-        {bigList[index].name} - {bigList[index].email}
-      </p>
-    </div>
-  );
+  const [login, setLogin] = useState("moontahoe");
 
   return (
-    <FixedSizeList
-      height={window.innerHeight}
-      width={window.innerWidth - 20}
-      itemCount={bigList.length}
-      itemSize={50}
-    >
-      {renderRow}
-    </FixedSizeList>
+    <>
+      {/* <SearchForm value={login} onSearch={setLogin} /> */}
+      <GitHubUser login={login} />
+    </>
   );
 }
 
