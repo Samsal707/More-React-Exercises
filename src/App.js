5f8200426822ea68c020b8a0a2e41dfe251e0dc1
Samsal707 <samsal707@gmail.com>
2022-12-29 17:29:00 +1100

    React - working with lists - windowing/virtualization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
+import List from  "./List"
 
-import GitHubUser from './GitHubUser';
-import './App.css';
+const tahoe_peaks = [
+  { name: "Freel Peak", elevation: 10891 },
+  { name: "Monument Peak", elevation: 10067 },
+  { name: "Pyramid Peak", elevation: 9983 },
+  { name: "Mt. Tallac", elevation: 9735 }
+];
 
 export default function App() {
-  return <GitHubUser login="moonhighway" />;
+  return (
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={item => (
+        <>
+          {item.name} - {item.elevation.toLocaleString()}ft
+        </>
+      )}
+    />
+  );
 }
