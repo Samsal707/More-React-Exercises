5183db8dda933b7d11e2b45a6fcf310fbf6a4481
Samsal707 <samsal707@gmail.com>
2022-12-31 16:05:54 +1100

    using useMountedRef with network requests

--- src/RepositoryReadme.js
+++ src/RepositoryReadme.js
@@ -4,24 +4,29 @@ import React, {
   useCallback
 } from "react";
 import ReactMarkdown from "react-markdown";
+import { useMountedRef } from "./useMountedRef";
 
 export default function RepositoryReadme({ repo, login }) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
   const [markdown, setMarkdown] = useState("");
 
-  const loadReadme = useCallback(async (login, repo) => {
-    setLoading(true);
-    const uri = `https://api.github.com/repos/${login}/${repo}/readme`;
-    const { download_url } = await fetch(uri).then(res =>
-      res.json()
-    );
-    const markdown = await fetch(download_url).then(res =>
-      res.text()
-    );
+  const mounted = useMountedRef();
+
+const loadReadme = useCallback(async (login, repo) => {
+  setLoading(true);
+  const uri = `https://api.github.com/repos/${login}/${repo}/readme`;
+  const { download_url } = await fetch(uri).then(res =>
+    res.json()
+  );
+  const markdown = await fetch(download_url).then(res =>
+    res.text()
+  );
+  if (mounted.current) {
     setMarkdown(markdown);
     setLoading(false);
-  }, []);
+  }
+}, []);
 
   useEffect(() => {
     if (!repo || !login) return;
